5f759eb7c9d149bfbebede92fe14529f90e23d34
Habib Basher <habib0904010@gmail.com>
2018-10-03 17:57:27 +0600

    template inheritance

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -19,4 +19,14 @@ def home(request):
             random.randint(0, 10000000)
         ]
     }
-    return render(request, 'base.html', context)
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    context = {}
+    return render(request, 'about.html', context)
+
+
+def contact(request):
+    context = {}
+    return render(request, 'contact.html', context)
