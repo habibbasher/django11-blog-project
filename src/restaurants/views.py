4f18d0c9215def233da0f2e732b68e9e02f03dc0
Habib Basher <habib0904010@gmail.com>
2018-10-04 18:06:29 +0600

    Restaurant list view and detail view with unique slug

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,43 +1,48 @@
+from django.db.models import Q
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView
-import random
-from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic import TemplateView, ListView, DetailView
 
 
-def about(request):
-    context = {}
-    return render(request, 'about.html', context)
+from .models import RestaurantLocation
 
 
-def contact(request):
-    context = {}
-    return render(request, 'contact.html', context)
+def restaurants_listview(request):
+    template_name = 'restaurants/restaurants_list.html'
+    queryset = RestaurantLocation.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, template_name, context)
 
 
-class HomeView(TemplateView):
-    template_name = 'home.html'
+class RestaurantListView(ListView):
+    """
+        Since model name is RestaurantLocation and it is a list view so if
+        we name the html file as restaurantlocation_list then django will
+        detect template_name automatically
+    """
+    # template_name = 'restaurants/restaurantlocation_list.html'
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(HomeView, self).get_context_data(*args, **kwargs)
-        num = random.randint(0, 10000000)
-        context = {
-            'data_from_py': True,
-            'num': num,
-            'some_list': [
-                num,
-                random.randint(0, 10000000),
-                random.randint(0, 10000000)
-            ]
-        }
-        return context
+    def get_queryset(self):
+        slug = self.kwargs.get('slug')
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                Q(category__iexact=slug)
+                | Q(category__icontains=slug)
+            )
+        else:
+            queryset = RestaurantLocation.objects.all()
+
+        return queryset
 
 
-# class AboutView(TemplateView):
-#     template_name = 'about.html'
-#
-#
-# class ContactView(TemplateView):
-#     template_name = 'contact.html'
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all()
+
+    # def get_object(self, *args, **kwargs):
+    #     rest_id = self.kwargs.get('rest_id')
+    #     # Also pk=rest_id can be used
+    #     obj = get_object_or_404(RestaurantLocation, id=rest_id)
+    #     return obj
