4173f2d0ac4b843631f9c84f42566b906ff30efd
Habib Basher <habib0904010@gmail.com>
2018-10-04 09:54:50 +0600

    class based view with template view implementation

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,25 +1,10 @@
 from django.http import HttpResponse
+from django.views import View
+from django.views.generic import TemplateView
 import random
 from django.shortcuts import render
 
 # Create your views here.
-
-# function based view
-
-
-def home(request):
-
-    num = random.randint(0, 10000000)
-    context = {
-        'data_from_py': True,
-        'num': num,
-        'some_list': [
-            num,
-            random.randint(0, 10000000),
-            random.randint(0, 10000000)
-        ]
-    }
-    return render(request, 'home.html', context)
 
 
 def about(request):
@@ -30,3 +15,29 @@ def about(request):
 def contact(request):
     context = {}
     return render(request, 'contact.html', context)
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        num = random.randint(0, 10000000)
+        context = {
+            'data_from_py': True,
+            'num': num,
+            'some_list': [
+                num,
+                random.randint(0, 10000000),
+                random.randint(0, 10000000)
+            ]
+        }
+        return context
+
+
+# class AboutView(TemplateView):
+#     template_name = 'about.html'
+#
+#
+# class ContactView(TemplateView):
+#     template_name = 'contact.html'
